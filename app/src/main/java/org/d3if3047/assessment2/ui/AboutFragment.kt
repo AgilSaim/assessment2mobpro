8afe8a6d87c6282c3cd4ca9f8db190e4eff8bde1
Agil Saim Setiawan <agilsaimsetiawan23@gmail.com>
2023-05-21 23:14:03 +0700

    2.2 Membuat navigasi menggunakan menu & 2.3 Membuat navigasi ke app lain

---
+++ app/src/main/java/org/d3if3047/assessment2/ui/AboutFragment.kt
@@ -0,0 +1,7 @@
+package org.d3if3047.assessment2.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if3047.assessment2.R
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+}
--- app/src/main/java/org/d3if3047/assessment2/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if3047/assessment2/ui/HitungFragment.kt
@@ -1,13 +1,13 @@
 package org.d3if3047.assessment2.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import org.d3if3047.assessment2.R
 import org.d3if3047.assessment2.databinding.FragmentHitungBinding
 import org.d3if3047.assessment2.model.HasilDiskon
@@ -22,12 +22,42 @@ class HitungFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentHitungBinding.inflate(layoutInflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.button.setOnClickListener { hitungDiskon() }
+        binding.shareButton.setOnClickListener { shareData() }
+
         viewModel.getHasilDiskon().observe(requireActivity(), { showResult(it) })
+    }
+
+    private fun shareData() {
+        val message = getString(R.string.bagikan_template,
+            binding.hargaEditText.text,
+            binding.diskonEditText.text,
+            binding.totalTextView.text,
+        )
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT, message)
+        if (shareIntent.resolveActivity(
+                requireActivity().packageManager) != null) {
+            startActivity(shareIntent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun hitungDiskon() {
@@ -50,5 +80,6 @@ class HitungFragment : Fragment() {
     private fun showResult(result: HasilDiskon?) {
         if (result == null) return
         binding.totalTextView.text = getString(R.string.total, result.total.toString())
+        binding.buttonGroup.visibility = View.VISIBLE
     }
 }
