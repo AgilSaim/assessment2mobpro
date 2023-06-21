20dcf2142b3ad01a4d8567732e4f085e57c1cd98
Agil Saim Setiawan <agilsaimsetiawan23@gmail.com>
2023-06-21 08:01:13 +0700

    Menambahkan notifikasi

--- app/src/main/java/org/d3if3047/assessment2/ui/barang/BarangFragment.kt
+++ app/src/main/java/org/d3if3047/assessment2/ui/barang/BarangFragment.kt
@@ -1,13 +1,19 @@
 package org.d3if3047.assessment2.ui.barang
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import org.d3if3047.assessment2.MainActivity
 import org.d3if3047.assessment2.databinding.FragmentBarangBinding
 import org.d3if3047.assessment2.network.BarangApi
 
@@ -51,11 +57,30 @@ class BarangFragment : Fragment() {
             }
             BarangApi.ApiStatus.SUCCESS -> {
                 binding.progressBar.visibility = View.GONE
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestNotificationPermission()
+                }
             }
             BarangApi.ApiStatus.FAILED -> {
                 binding.progressBar.visibility = View.GONE
                 binding.networkError.visibility = View.VISIBLE
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
         }
     }
 }
